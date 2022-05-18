60224c96bc73701f460ea850a2ac2688432bc781
Sergey Shamardin <89478892+Jason0024@users.noreply.github.com>
2022-05-18 23:59:40 +0700

    bug fix

--- blocks/element-grid/__like/element-grid__like.js
+++
@@ -1,6 +0,0 @@
-
-let likes = document.querySelectorAll('.element-grid__like');
-
-for (let i = 0 ; i < likes.length; i++) {
-  likes[i].addEventListener('click', function(){ likes[i].classList.toggle('element-grid__like_active'); });
-}
--- scripts/index.js
+++ scripts/index.js
@@ -40,3 +40,11 @@ function formSubmitHandler (evt) {
 
 // Прикрепляем обработчик к форме
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+//Like
+let likes = document.querySelectorAll('.element-grid__like');
+
+for (let i = 0 ; i < likes.length; i++) {
+  likes[i].addEventListener('click', function(){ likes[i].classList.toggle('element-grid__like_active'); });
+}
