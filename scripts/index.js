773e0ea9bf8b2306e9d5ee6c24495c13c39eae12
Sergey Shamardin <89478892+Jason0024@users.noreply.github.com>
2022-05-31 04:12:50 +0700

    feat/added-detele-button & made logic

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 const addButton  = document.querySelector('.add-button');
 const closeButton = document.querySelectorAll('.popup__close');
 const editButton = document.querySelector('.profile__edit');
+const deleteButton = document.querySelector('.element-grid__delete');
 
 // Шаблон для фото
 const photoContainer = document.querySelector('.element-grid');
@@ -23,6 +24,8 @@ const profileJob = document.querySelector('.profile__job');
 const formPhoto = document.querySelector('.popup__form_photo-type');
 const photoTitleInput = document.querySelector('.popup__input_title_type');
 const photoLinkInput = document.querySelector('.popup__input_src_type');
+
+const elementTemplate = document.querySelector('.element-grid-template').content;
 
 const initialCards = [
   {
@@ -50,6 +53,71 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+// Инициализируем карточки
+function addPhoto(name, link) {
+  function handleDeletePhoto(e) {
+      e.target.closest('.element-grid__item').remove();
+  }
+
+  function handleLike(e) {
+    e.target.classList.toggle('element-grid__like_active');
+  }
+
+  const newPhoto = photoTemplate.content.querySelector('.element-grid__item').cloneNode(true);
+  const photoSrc = newPhoto.querySelector('.element-grid__pic');
+  const photoTitle = newPhoto.querySelector('.element-grid__title');
+  const deleteButton = newPhoto.querySelector('.element-grid__delete');
+  const likeButton = newPhoto.querySelector('.element-grid__like');
+
+  photoTitle.textContent = name;
+  photoSrc.alt = name;
+  photoSrc.src = link;
+
+  function handleModal() {
+
+    const popupModal = document.querySelector('.popup_type_modal');
+    const modalSrc = popupModal.querySelector('.popup__pic');
+    const modalTitle = popupModal.querySelector('.popup__caption');
+
+    popupOpen(popupModal);
+    modalTitle.textContent = name;
+    modalSrc.alt = name;
+    modalSrc.src = link;
+
+  }
+
+  deleteButton.addEventListener('click', handleDeletePhoto);
+  likeButton.addEventListener('click', handleLike);
+  photoSrc.addEventListener('click', handleModal);
+
+  return newPhoto;
+}
+
+initialCards.forEach(function(item) {
+  const newCard = addPhoto(item['name'], item['link']);
+  photoContainer.append(newCard);
+});
+
+// Обработчик «отправки» формы
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    // Форма добавления фото
+    if (photoTitleInput.value && photoLinkInput.value) {
+      photoContainer.prepend(addPhoto(photoTitleInput.value, photoLinkInput.value));
+      // Очищаем поля
+      photoTitleInput.value = '';
+      photoLinkInput.value = '';
+    }
+
+    // Закрываем попап
+    popupClose();
+}
 
 // Открытие попапа
 function openPopup(event) {
@@ -85,40 +153,14 @@ function closePopup(popup) {
   popup.classList.remove("popup_open")
 };
 
+
+
+
+
+
 //События
 addButton.addEventListener('click',openPopupPhoto);
 editButton.addEventListener('click', openPopupProfile);
 
 formProfile.addEventListener('submit', formSubmitHandlerProfile);
-
-
-// Инициализируем карточки
-function addPhoto(name, link) {
-
-  const newPhoto = photoTemplate.content.querySelector('.element-grid__item').cloneNode(true);
-  const photoSrc = newPhoto.querySelector('.element-grid__pic');
-  const photoTitle = newPhoto.querySelector('.element-grid__title');
-
-  photoTitle.textContent = name;
-  photoSrc.src = link;
-
-  return newPhoto;
-
-}
-
-initialCards.forEach(item => {
-  const newCard = addPhoto(item['name'], item['link']);
-  photoContainer.append(newCard);
-});
-
-
-
-
-//Like
-let likes = document.querySelectorAll('.element-grid__like');
-
-for (let i = 0 ; i < likes.length; i++) {
-  likes[i].addEventListener('click', function(){
-     likes[i].classList.toggle('element-grid__like_active');
-   });
-}
+formPhoto.addEventListener('submit', formSubmitHandler);
